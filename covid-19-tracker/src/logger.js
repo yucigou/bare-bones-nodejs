cf056cabe65b55dbcbd0a3b72b344dfbc257468a
Yuci Gou <yuci.gou@gmail.com>
2020-05-08 23:29:51 +0100

    Add a logger

--- covid-19-tracker/src/logger.js
+++ covid-19-tracker/src/logger.js
@@ -1,12 +1,14 @@
-const logger = require('winston');
+const winston = require('winston');
 require('winston-daily-rotate-file');
 
-logger.add(logger.transports.DailyRotateFile, {
-  dirname: './logs',
-  filename: 'covid-19-tracker.log',
-  datePattern: 'YYYY-MM-DD',
-  zippedArchive: true,
-  maxFiles: '30d',
-});
+winston.add(
+  new winston.transports.DailyRotateFile({
+    dirname: './logs',
+    filename: 'covid-19-tracker.log',
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxFiles: '30d',
+  })
+);
 
-module.exports = logger;
+module.exports = winston;
